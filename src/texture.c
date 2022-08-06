0ba3f617729044b3c8b94cc0f8c54d83e3c585ed
Shuntaro Nishizawa <me@shun.technology>
2022-08-07 02:57:28 +0900

    feat: Change img name

--- src/texture.c
+++ src/texture.c
@@ -8,10 +8,10 @@
 texture_t textures[TEXTURE_LENGTH];
 
 void textureInit() {
-  char pathStr[16];
+  char pathStr[32];
 
   for (int i = 0; i < TEXTURE_LENGTH; i++) {
-    snprintf(pathStr, 16, "./assets/%d.png", i);
+    snprintf(pathStr, 32, "./assets/img%d.png", i);
     textures[i].id =
         pngBind(pathStr, PNG_NOMIPMAP, PNG_ALPHA, &textures[i].info, GL_CLAMP,
                 GL_NEAREST, GL_NEAREST);
